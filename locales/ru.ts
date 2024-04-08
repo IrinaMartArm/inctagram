0cbd62b2a2bca303b590a710bb605eaa4850eccc
Ирина Мартоян <surenira@icloud.com>
2024-04-08 21:17:13 +0400

    feat: add icons

---
+++ locales/ru.ts
@@ -0,0 +1,21 @@
+export const ru = {
+  favorites: {
+    title: "Страница",
+  },
+  general: {
+    title: "Страница",
+  },
+  signup: {
+    1: "Условиями обслуживания",
+    2: "Политикой конфиденциальности",
+    agree: "Я согласен с <1></1> и <2></2>",
+    name: "Имя пользователя",
+    password: "Пароль",
+    password2: "Подтверждение пароля",
+    question: "У вас уже есть аккаунт?",
+    signin: "Войти",
+    title: "Зарегистрироваться",
+  },
+};
+
+export type LocaleType = typeof ru;
