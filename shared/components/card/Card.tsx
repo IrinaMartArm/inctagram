4842241396cbf344c927131c95a8593551ed0cf9
Ирина Мартоян <surenira@icloud.com>
2024-04-08 21:10:39 +0400

    feat: add Card component

---
+++ shared/components/card/Card.tsx
@@ -0,0 +1,35 @@
+import {
+  ComponentPropsWithoutRef,
+  ElementRef,
+  ElementType,
+  LegacyRef,
+  ReactNode,
+  Ref,
+  forwardRef,
+} from "react";
+
+import { clsx } from "clsx";
+
+import s from "./card.module.scss";
+
+type CardProps<T extends ElementType> = {
+  as?: T;
+  children?: ReactNode;
+  className?: string;
+  ref?: LegacyRef<HTMLElement>;
+} & ComponentPropsWithoutRef<T>;
+
+export const Card = forwardRef(
+  <T extends ElementType = "div">(
+    props: CardProps<T>,
+    ref: Ref<ElementRef<T>>,
+  ) => {
+    const { as: Component = "div", children, className, ...rest } = props;
+
+    return (
+      <Component className={clsx(s.card, className)} {...rest} ref={ref}>
+        {children}
+      </Component>
+    );
+  },
+);
