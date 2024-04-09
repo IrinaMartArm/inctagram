ecb48a97b8b3190f5b7eb160c95de16fe6e2adc7
Ирина Мартоян <surenira@icloud.com>
2024-04-09 12:35:20 +0400

    feat: create private routes

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,13 +1,16 @@
+import { WithNavigate } from "@/shared/assets/hoc/WithNavigate";
 import { PageWrapper } from "@/shared/components";
 import { HeadMeta } from "@/shared/components/headMeta/HeadMeta";
 import { getMainLayout } from "@/shared/components/layout/mainLayout/MainLayout";
 
 const Profile = () => {
   return (
-    <PageWrapper>
-      <HeadMeta title={"Profile"} />
-      <div>Profile</div>
-    </PageWrapper>
+    <WithNavigate>
+      <PageWrapper>
+        <HeadMeta title={"Profile"} />
+        <div>Profile</div>
+      </PageWrapper>
+    </WithNavigate>
   );
 };
 
