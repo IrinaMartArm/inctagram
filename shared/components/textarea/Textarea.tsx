54c7c14341f8db6cab18fd0d938c326dddfe7f85
Andrew Chernoff <whitedrew538@gmail.com>
2024-04-09 18:29:10 +0500

    fix: change stories path

---
+++ shared/components/textarea/Textarea.tsx
@@ -0,0 +1,5 @@
+import s from "./textarea.module.scss";
+
+export const Textarea = () => {
+  return <div className={s.textarea}>Textarea</div>;
+};
---
+++ shared/components/textarea/textarea.stories.ts
@@ -0,0 +1,17 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import { Textarea } from "./Textarea";
+
+const meta = {
+  argTypes: {},
+  component: Textarea,
+  tags: ["autodocs"],
+  title: "Components/Textarea",
+} satisfies Meta<typeof Textarea>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const TextareaDemo: Story = {
+  args: {},
+};
