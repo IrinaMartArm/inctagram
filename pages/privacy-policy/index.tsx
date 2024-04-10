71c729ee5f2a00fbfef0cec6e7291e22d2f8c043
Ирина Мартоян <surenira@icloud.com>
2024-04-10 23:38:31 +0400

    test

--- pages/privacy-policy/index.tsx
+++ pages/privacy-policy/index.tsx
@@ -1,12 +1,15 @@
+import { useTranslation } from "@/shared/assets/hooks/useTranslation";
 import { PageWrapper } from "@/shared/components";
 import { HeadMeta } from "@/shared/components/headMeta/HeadMeta";
 import { getMainLayout } from "@/shared/components/layout/mainLayout/MainLayout";
 
 const PrivacyPolicy = () => {
+  const { t } = useTranslation();
+
   return (
     <PageWrapper>
       <HeadMeta title={"Privacy Policy"} />
-      <div>Privacy Policy</div>
+      <div>{t.policy.title}</div>
     </PageWrapper>
   );
 };
