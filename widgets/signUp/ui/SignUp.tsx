570a30917429d62ea4952e95920efe63706e4edb
Ирина Мартоян <surenira@icloud.com>
2024-04-10 22:49:23 +0400

    INC-27 feat: ui-kit Sign-Up

--- widgets/signUp/ui/SignUp.tsx
+++ widgets/signUp/ui/SignUp.tsx
@@ -1,25 +1,61 @@
+import { useForm } from "react-hook-form";
+
 import { GitHubBig, Google } from "@/public";
 import { useTranslation } from "@/shared/assets/hooks/useTranslation";
+import {
+  Button,
+  ControlledCheckBox,
+  Input,
+  Typography,
+} from "@/shared/components";
 import { Card } from "@/shared/components/card";
 import { Trans } from "@/shared/components/trans/Trans";
+import Link from "next/link";
+
+import s from "./signUp.module.scss";
 
 export const SignUpCard = () => {
   const { t } = useTranslation();
+  const { control } = useForm();
 
   return (
-    <Card>
-      <div>{t.signup.title}</div>
-      <div>
+    <Card as={"form"}>
+      <Typography variant={"h1"}>{t.signup.title}</Typography>
+      <div className={s.socials}>
         <Google />
         <GitHubBig />
       </div>
-      <Trans
-        tags={{
-          1: () => <button style={{ color: "red" }}>{t.signup["1"]}</button>,
-          2: () => <button style={{ color: "red" }}>{t.signup["2"]}</button>,
-        }}
-        text={t.signup.agree}
+      <Input label={"Username"} placeholder={"Username"} type={"text"} />
+      <Input label={"Email"} placeholder={"Email"} type={"email"} />
+      <Input label={"Password"} placeholder={"Password"} type={"password"} />
+      <Input
+        label={"Password confirmation"}
+        placeholder={"Password confirmation"}
+        type={"password"}
       />
+      <div className={s.policy}>
+        <ControlledCheckBox control={control} name={"agree"} />
+        <Trans
+          tags={{
+            1: () => (
+              <Link href={"./terms-of-service"}>
+                <Typography variant={"small_link"}>{t.signup["1"]}</Typography>
+              </Link>
+            ),
+            2: () => (
+              <Link href={"./privacy-policy"}>
+                <Typography variant={"small_link"}>{t.signup["2"]}</Typography>
+              </Link>
+            ),
+          }}
+          text={t.signup.agree}
+        />
+      </div>
+      <div className={s.button}>
+        <Button fullWidth>{t.signup.title}</Button>
+      </div>
+      <Typography variant={"regular_text-16"}>{t.signup.question}</Typography>
+      <Button variant={"link"}>{t.signup.signIn}</Button>
     </Card>
   );
 };
