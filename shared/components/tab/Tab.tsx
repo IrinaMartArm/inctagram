d627ad34babf497f146dfdb5962e5bce18d557b4
Oksana <oksana.s.kovalchuk@gmail.com>
2024-04-10 11:22:52 +0300

    INC-25 feat: create tab component

---
+++ shared/components/tab/Tab.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+
+import { Typography } from "@/shared/components";
+import * as Tabs from "@radix-ui/react-tabs";
+
+import s from "./tab.module.scss";
+
+export type TabOptions = {
+  disabled: boolean;
+  title: string;
+  value: string;
+};
+
+type Props = {
+  defaultValue?: string;
+  disabled?: boolean;
+  label: string;
+  onChange?: (value: string) => void;
+  options: TabOptions[];
+  value?: string;
+};
+
+export const Tab: FC<Props> = ({
+  defaultValue,
+  disabled,
+  label,
+  onChange,
+  options,
+  value,
+}) => (
+  <Tabs.Root defaultValue={defaultValue} onValueChange={onChange} value={value}>
+    <Tabs.List aria-label={label}>
+      {options.map((option) => (
+        <Tabs.Trigger
+          className={s.tabsTrigger}
+          disabled={option.disabled || disabled}
+          key={option.value}
+          value={option.value}
+        >
+          <Typography variant={"subtitle2"}>{option.title}</Typography>
+        </Tabs.Trigger>
+      ))}
+    </Tabs.List>
+  </Tabs.Root>
+);
