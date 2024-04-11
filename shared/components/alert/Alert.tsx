9a639c13e48b2428b092f0e970f98721800489a2
Oksana <oksana.s.kovalchuk@gmail.com>
2024-04-11 12:11:08 +0300

    INC-23 feat: create alert component

---
+++ shared/components/alert/Alert.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+
+import { Close } from "@/public";
+import { Typography } from "@/shared/components";
+import { clsx } from "clsx";
+
+import s from "./alert.module.scss";
+
+type AlertVariant = "error" | "success";
+
+type Props = {
+  className?: string;
+  onClick?: () => void;
+  title: string;
+  variant: AlertVariant;
+};
+
+export const Alert: FC<Props> = ({ className, onClick, title, variant }) => {
+  const alertCN = clsx(s.alert, s[variant], className);
+
+  return (
+    <div className={alertCN}>
+      <div className={s.text}>
+        {variant === "error" && (
+          <Typography variant={"Bold_text-16"}>Error!</Typography>
+        )}
+        <Typography variant={"regular_text-16"}>{title}</Typography>
+      </div>
+      <button aria-label={"close alert"} onClick={onClick}>
+        <Close />
+      </button>
+    </div>
+  );
+};
