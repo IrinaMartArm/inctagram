243ecbbf5443946896065f143e1ba42055adc163
Oksana <oksana.s.kovalchuk@gmail.com>
2024-04-15 09:56:04 +0300

    INC-26 feat: make stgnIn, add styles

--- widgets/signIn/ui/SignIn.tsx
+++ widgets/signIn/ui/SignIn.tsx
@@ -1,17 +1,69 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+
 import { GitHubBig, Google } from "@/public";
 import { useTranslation } from "@/shared/assets/hooks/useTranslation";
-import { Card } from "@/shared/components";
+import {
+  Button,
+  Card,
+  ControlledTextField,
+  Typography,
+} from "@/shared/components";
+import Link from "next/link";
+
+import s from "./signIn.module.scss";
+
+type SignInData = {
+  email: string;
+  password: string;
+};
 
 export const SignInCard = () => {
   const { t } = useTranslation();
+  const { forgotPassword, password, question, signUp, title } = t.signIn;
+  const { control, handleSubmit, register } = useForm<SignInData>();
+
+  const onSubmit: SubmitHandler<SignInData> = (data) => {
+    console.log(data);
+  };
 
   return (
-    <Card>
-      <div>{t.signIn.title}</div>
-      <div>
+    <Card as={"form"} onSubmit={handleSubmit(onSubmit)}>
+      <Typography as={"h1"} className={s.title} variant={"h1"}>
+        {title}
+      </Typography>
+      <div className={s.socials}>
         <Google />
         <GitHubBig />
       </div>
+      <ControlledTextField
+        control={control}
+        {...register("email")}
+        label={"Email"}
+        placeholder={"Email"}
+        type={"email"}
+      />
+      <ControlledTextField
+        control={control}
+        {...register("password")}
+        label={password}
+        placeholder={password}
+        type={"password"}
+      />
+
+      <Link className={s.link} href={"./sign-up"}>
+        <Typography className={s.forgotPassword} variant={"regular_text-14"}>
+          {forgotPassword}
+        </Typography>
+      </Link>
+      <Button fullWidth type={"submit"}>
+        {title}
+      </Button>
+      <Typography className={s.question} variant={"regular_text-16"}>
+        {question}
+      </Typography>
+      <Button as={"a"} fullWidth type={"button"} variant={"link"}>
+        {signUp}
+      </Button>
     </Card>
   );
 };
