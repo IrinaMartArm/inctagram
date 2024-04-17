eb43480f9c8ff31a5712458e739f8f52621c6b0c
Briws <brightwiths@gmail.com>
2024-04-18 01:12:04 +0700

    INC-20 feat: updated styles

--- shared/components/DayPicker/DayPicker.tsx
+++ shared/components/DayPicker/DayPicker.tsx
@@ -1,9 +1,12 @@
 import React, { useState } from "react";
 import { DayPicker as ReactDayPicker } from "react-day-picker";
 
-import { format } from "date-fns";
+import { Input } from "@/shared/components";
 
+import "./DayPicker.scss";
 import "react-day-picker/dist/style.css";
+
+import s from "./DayPicker.module.scss";
 
 type ValuePiece = Date | null;
 
@@ -12,52 +15,36 @@ type Value = [ValuePiece, ValuePiece] | ValuePiece;
 export const DayPicker = () => {
   const [selected, setSelected] = useState<Date>();
 
-  let footer = <p>Please pick a day.</p>;
+  const onClick = () => {
+    alert("onClick");
+  };
 
-  if (selected) {
-    footer = <p>You picked {format(selected, "PP")}.</p>;
-  }
+  const dateFormated = selected
+    ? `${selected.getDate()}/${selected.getMonth() + 1}/${selected.getFullYear()}`
+    : "";
 
-  const css = `
-  .my-selected { 
-     background-color: #234E99;
-  }
-  .my-selected:hover:not([disabled]) { 
-    border-color: blue;
-    color: blue;
-  }
-  .my-today { 
-    background-color: #171717;
-    color: #234E99;
-  }
-  .my-disabled {
-      background-color: red;
-      color: #234E99; 
-  }
-`;
+  const weekends = [5, 6];
+  const weekendStyle = { color: "#F23D61" };
 
   return (
     <>
-      <style>{css}</style>
+      <div className={s.inputContainer}>
+        <Input
+          isShowButton
+          label={"Date select"}
+          onChange={() => {}}
+          onClick={onClick}
+          type={"text"}
+          value={dateFormated}
+        />
+      </div>
       <ReactDayPicker
         mode={"single"}
-        modifiersClassNames={{
-          disabled: "my-disabled",
-          selected: "my-selected",
-          today: "my-today",
-        }}
+        modifiers={{ weekend: (day) => weekends.includes(day.getDay()) }}
+        modifiersStyles={{ weekend: weekendStyle }}
         onSelect={setSelected}
         selected={selected}
-        styles={{
-          caption: { color: "#FFFFFF" },
-          nav_button_next: { background: "#4C4C4C" },
-          nav_button_previous: { background: "#4C4C4C" },
-          root: { background: "#171717", maxWidth: "fit-content" },
-          table: { color: "#FFFFFF" },
-        }}
-        /*modifiersClassNames={{
-        today: s.today
-        }}*/
+        showOutsideDays
       />
     </>
   );
