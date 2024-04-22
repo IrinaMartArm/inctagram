660a233e7631b61349aea828fbf70413d2b68e9a
Ирина Мартоян <surenira@icloud.com>
2024-04-22 14:17:01 +0400

    INC-28 fix: Loader styles

--- shared/components/loader/Loader.tsx
+++ shared/components/loader/Loader.tsx
@@ -1,4 +1,8 @@
 import s from "./loader.module.scss";
 export const Loader = () => {
-  return <div className={s.loader}></div>;
+  return (
+    <div className={s.root}>
+      <div className={s.loader}></div>
+    </div>
+  );
 };
