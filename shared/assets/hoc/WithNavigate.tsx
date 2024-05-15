faf49514f0992db662b8e2c479a7d83711c0e73e
Ирина Мартоян <surenira@icloud.com>
2024-05-15 22:04:11 +0400

    INC-29 refactor: router

--- shared/assets/hoc/WithNavigate.tsx
+++ shared/assets/hoc/WithNavigate.tsx
@@ -1,31 +1,45 @@
 import { FC, PropsWithChildren, useEffect } from "react";
 
 import { useMeQuery } from "@/shared/assets/api/auth/auth-api";
-import { Paths, commonRoutes } from "@/shared/assets/paths";
+import { Paths, authRoutes, commonRoutes } from "@/shared/assets/paths";
 import { Loader } from "@/shared/components";
 import { useRouter } from "next/router";
 
 export const WithNavigate: FC<PropsWithChildren<{}>> = ({ children }) => {
   const router = useRouter();
-  const { data: isAuth, isLoading } = useMeQuery();
+  const { data: isAuth, isLoading } = useMeQuery(undefined);
+
+  const userId = isAuth?.userId;
 
   const remainingPath: string = router.pathname.replace(
     /^\/profile(\/[^/]+)?|\/profile\?(.+)/,
-    "/profile",
+    Paths.PROFILE + `/${userId}`,
   );
 
-  const isProtectedPage: boolean = !commonRoutes.includes(remainingPath);
+  const isProtectedPage: boolean =
+    !commonRoutes.includes(remainingPath) &&
+    !authRoutes.includes(remainingPath);
+
+  const isAuthPage: boolean =
+    !commonRoutes.includes(remainingPath) && !remainingPath;
 
   useEffect(() => {
     if (
-      (!isLoading && !isAuth && isProtectedPage) ||
-      router.pathname === "./github"
+      !isLoading &&
+      !isAuth &&
+      isProtectedPage
+      // router.pathname === "/github"
     ) {
       router.push(Paths.MAIN);
     }
+    if (!isLoading && isAuth && isAuthPage) {
+      const userId = isAuth?.userId;
+
+      router.push(Paths.PROFILE + `/${userId}`);
+    }
 
     return;
-  }, [isAuth, isProtectedPage, router, isLoading]);
+  }, [isAuth, isProtectedPage, router, isLoading, isAuthPage]);
 
   if (isLoading) {
     return <Loader />;
