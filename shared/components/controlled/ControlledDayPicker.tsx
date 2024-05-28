06cc78154354c218eec277cec8210809dbda5392
Ирина Мартоян <surenira@icloud.com>
2024-05-28 22:40:10 +0400

    INC-29 fix

--- shared/components/controlled/ControlledDayPicker.tsx
+++ shared/components/controlled/ControlledDayPicker.tsx
@@ -4,7 +4,8 @@ import {
   useController,
 } from "react-hook-form";
 
-import { DayPicker, DayPickerProps } from "@/shared/components/dayPicker";
+import { DayPicker } from "@/shared/components/dayPicker";
+import { DayPickerProps } from "@/shared/components/dayPicker/DayPicker";
 
 type PropsType<T extends FieldValues> = UseControllerProps<T> &
   Omit<DayPickerProps, "onChange" | "selected">;
