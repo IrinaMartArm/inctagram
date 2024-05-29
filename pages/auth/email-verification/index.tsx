505ea2fe31db300294797cfe33110aa8da0f3484
Ирина Мартоян <surenira@icloud.com>
2024-05-29 21:52:03 +0400

    INC-29 feat: add icons

--- pages/auth/email-verification/index.tsx
+++
@@ -1,9 +0,0 @@
-import { getLayout } from "@/shared/components/layout/baseLayout/BaseLayout";
-import { Verification } from "@/widgets";
-
-const VerificationPage = () => {
-  return <Verification />;
-};
-
-VerificationPage.getLayout = getLayout;
-export default VerificationPage;
