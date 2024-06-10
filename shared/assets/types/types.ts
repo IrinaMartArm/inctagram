6894ae308dd8aaf86a848718273ff495ba9f9cd8
Ирина Мартоян <surenira@icloud.com>
2024-06-10 21:32:50 +0400

    INC-51 feat: common

--- shared/assets/types/types.ts
+++ shared/assets/types/types.ts
@@ -4,7 +4,6 @@ export type Nullable<T> = T | null;
 
 export type IconProps = {
   color?: string;
-  colorB?: string;
   ref?: LegacyRef<SVGSVGElement>;
   size?: number;
 } & Omit<HTMLProps<HTMLSpanElement>, "color" | "size">;
