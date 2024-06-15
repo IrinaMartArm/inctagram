6cf43d89c71b523959dbbfc4bec6ed41ca579095
Ирина Мартоян <surenira@icloud.com>
2024-06-15 22:19:29 +0400

    INC-51 in progress

--- pages/google-auth/index.tsx
+++ pages/google-auth/index.tsx
@@ -1,14 +1,23 @@
 import { useEffect } from "react";
 
+import { Paths } from "@/shared/assets";
 import { setAccessToken } from "@/shared/assets/helpers/authentication";
 import { HeadMeta, Loader } from "@/shared/components";
+import { useRouter } from "next/router";
 
 const GoogleAuth = () => {
+  const router = useRouter();
+
   useEffect(() => {
     const queryString = window.location.search;
 
-    setAccessToken(queryString);
-  }, []);
+    if (queryString) {
+      setAccessToken(queryString);
+      router.push(Paths.PROFILE);
+    } else {
+      console.error("Token not found in URL");
+    }
+  }, [router]);
 
   return (
     <>
