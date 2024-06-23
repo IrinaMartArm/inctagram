7ded1ede34490a138716c427fbed79672883b8d6
Ирина Мартоян <surenira@icloud.com>
2024-06-23 23:50:10 +0400

    INC-51_2 feat: refactor

---
+++ shared/components/arrows/PrevArrowComponent.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import { PrevArrow } from '@/public'
+import { CropArg } from '@/shared/assets/types/types'
+import { Button } from '@/shared/components'
+
+import s from '@/widgets/addPhotoForm/ui/croppingPhoto/carousel/carousel.module.scss'
+type Props = {
+  callbackFunction?: () => void
+  croppedAreaPixels?: CropArg | null
+  ind: number
+  len?: number
+  onClick?: () => void
+  setInd: (val: number) => void
+}
+export const PrevArrowComponent = ({ callbackFunction, ind, len, onClick, setInd }: Props) => {
+  if (ind === 0 || (len ? len : 0) <= 1) {
+    return null
+  }
+  const handleClick = () => {
+    setInd(ind - 1)
+    if (callbackFunction) {
+      callbackFunction()
+    }
+    if (onClick) {
+      onClick()
+    }
+  }
+
+  return (
+    <Button className={s.prevArrow} onClick={handleClick} variant={'icon'}>
+      <PrevArrow />
+    </Button>
+  )
+}
---
+++ widgets/profile/general/validators/index.ts
@@ -0,0 +1,33 @@
+import z from 'zod'
+
+const sixteenYearsAgo = new Date()
+
+sixteenYearsAgo.setFullYear(sixteenYearsAgo.getFullYear() - 16)
+
+export const profileFormSchema = (t: any) =>
+  z.object({
+    aboutMe: z.string().max(200, t.errors.aboutMe).optional(),
+    city: z.string().optional(),
+    country: z.string().optional(),
+    dateOfBirth: z
+      .string()
+      .refine(
+        dateString => {
+          if (!dateString) {
+            return true
+          }
+          const dateOfBirth = new Date(dateString)
+
+          return dateOfBirth <= sixteenYearsAgo
+        },
+        {
+          message: 'ups!',
+        }
+      )
+      .optional(),
+    firstName: z.string().min(1, t.errors.firstName).max(50),
+    lastName: z.string().min(1, t.errors.lastName).max(50),
+    username: z.string().min(6).max(30),
+  })
+
+export type ProfileFormSchema = z.infer<ReturnType<typeof profileFormSchema>>
