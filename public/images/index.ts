4bf9bc55e150d8ad99d5e6ed4376c38b8287f2b3
Ирина Мартоян <surenira@icloud.com>
2024-06-24 13:25:37 +0400

    INC-51_2 common

---
+++ public/images/index.ts
@@ -0,0 +1,17 @@
+// import boy from './Boy.png'
+// import flagRussia from './FlagRussia.svg'
+// import flagUK from './FlagUK.svg'
+// import girl from './Girl.png'
+// import avatar from './avatar.png'
+// import datePicker from './datePicker.png'
+// import noAva from './noAva.webp'
+// export { avatar, boy, datePicker, flagRussia, flagUK, girl, noAva}
+
+
+export * from './Boy.png'
+export * from './FlagRussia.svg'
+export * from './FlagUK.svg'
+export * from './Girl.png'
+export * from './avatar.png'
+export * from './datePicker.png'
+export * from './noAva.webp'
