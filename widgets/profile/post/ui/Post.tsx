bb32e064698d8cddccd595682ed15edd0cff94df
Ирина Мартоян <surenira@icloud.com>
2024-06-24 10:41:07 +0400

    INC-51_2 refactor: refactor styles

--- widgets/profile/post/ui/Post.tsx
+++ widgets/profile/post/ui/Post.tsx
@@ -93,11 +93,3 @@ export const Post = () => {
     </div>
   )
 }
-
-const getInitials = (inputString: string) => {
-  const words = inputString.trim().split(/\s+/)
-  const firstInitial = words[0] ? words[0].charAt(0).toUpperCase() : ''
-  const secondInitial = words[1] ? words[1].charAt(0).toUpperCase() : ''
-
-  return firstInitial + secondInitial
-}
