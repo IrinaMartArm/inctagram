9d4a4c8605953531200f22ade1e0a1a401c81f77
Ирина Мартоян <surenira@icloud.com>
2024-07-04 15:57:33 +0400

    INC-51_2 fix

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,16 +1,8 @@
-import { useState } from 'react'
-
-import { useGetPostQuery, useGetPostsQuery } from '@/shared/assets/api/post/post-api'
-import { PostItemType } from '@/shared/assets/api/post/types'
 import { PageWrapper } from '@/shared/components'
 import { HeadMeta } from '@/shared/components/headMeta/HeadMeta'
 import { getMainLayout } from '@/shared/components/layout/mainLayout/MainLayout'
-import { useAddPhotoForm } from '@/widgets/addPhotoForm/hooks'
-import { Post } from '@/widgets/profile/post/ui/Post'
 
 const Home = () => {
-
-
   return (
     <PageWrapper>
       <HeadMeta title={'Posts'} />
