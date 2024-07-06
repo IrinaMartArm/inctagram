3ddbbc27b0ec36cc5c2eadf8d6a89644762f00b2
Gennadij Lobotskij <utyf185@mail.ru>
2024-07-06 16:53:25 +0300

    fix: filter img

--- entities/addPhotos/model/addPhoto-slice.ts
+++ entities/addPhotos/model/addPhoto-slice.ts
@@ -4,7 +4,6 @@ import { boolean } from 'zod'
 type CropImageWithFilter = {
   filter: string
   img: string
-  imgFile: any
 }
 export type ModalState = 'add-photo' | 'cropping' | 'filters' | 'publication'
 const slice = createSlice({
@@ -13,9 +12,7 @@ const slice = createSlice({
     cropImagesWithFilter: ([] as CropImageWithFilter[]) || null,
     images: ([] as string[]) || null,
     isOpen: false as boolean,
-    isPostCreated: false as boolean,
     modalState: 'add-photo' as ModalState,
-    post: {} as PostItemType,
   },
   name: 'addPhotoSlice',
   reducers: {
@@ -26,7 +23,6 @@ const slice = createSlice({
       state.cropImagesWithFilter[cropImageIndex] = {
         filter: 'none',
         img: cropImage,
-        imgFile: '',
       }
     },
     addImage: (state, action: PayloadAction<string>) => {
@@ -35,7 +31,6 @@ const slice = createSlice({
       state.cropImagesWithFilter.push({
         filter: 'none',
         img: action.payload,
-        imgFile: '',
       })
     },
     discardAll: state => {
@@ -55,28 +50,20 @@ const slice = createSlice({
         cropImageWithFilter: any
         cropImageWithFilterIndex: number
         filter: string
-        imgFile: any
       }>
     ) => {
-      const { cropImageWithFilter, cropImageWithFilterIndex, filter, imgFile } = action.payload
+      const { cropImageWithFilter, cropImageWithFilterIndex, filter } = action.payload
 
       state.cropImagesWithFilter[cropImageWithFilterIndex] = {
         filter: filter,
         img: cropImageWithFilter,
-        imgFile: imgFile,
       }
     },
     setIsOpen: (state, action: PayloadAction<boolean>) => {
       state.isOpen = action.payload
     },
-    setIsPostCreated: (state, action: PayloadAction<boolean>) => {
-      state.isPostCreated = action.payload
-    },
     setModalStateTo: (state, action: PayloadAction<ModalState>) => {
       state.modalState = action.payload
-    },
-    setPost: (state, action: PayloadAction<PostItemType>) => {
-      state.post = action.payload
     },
   },
 })
