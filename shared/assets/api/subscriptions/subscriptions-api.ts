25a231deada167b21eed6fcfd78398114c62f86e
Ирина Мартоян <surenira@icloud.com>
2024-08-20 16:35:14 +0400

    INC-52 feat: create SubscriptionsApi

---
+++ shared/assets/api/subscriptions/subscriptions-api.ts
@@ -0,0 +1,41 @@
+import { baseApi } from '@/shared/assets'
+import {
+  CreateSubscriptionArgs,
+  CreateSubscriptionResp,
+  CurrentSubscriptionResponse,
+  MyPaymentsArgs,
+  MyPaymentsResponse,
+} from '@/shared/assets/api/subscriptions/types'
+
+export const SubscriptionsApi = baseApi.injectEndpoints({
+  endpoints: builder => {
+    return {
+      createSubscription: builder.mutation<CreateSubscriptionResp, CreateSubscriptionArgs>({
+        invalidatesTags: ['Subscriptions'],
+        query: body => ({
+          body,
+          method: 'POST',
+          url: 'v1/subscriptions/create-payment',
+        }),
+      }),
+      getCurrentSubscription: builder.query<void, CurrentSubscriptionResponse>({
+        query: () => ({
+          method: 'GET',
+          url: `v1/subscriptions/current-subscription`,
+        }),
+      }),
+      getPayments: builder.query<MyPaymentsArgs, MyPaymentsResponse>({
+        query: ({ page, pageSize }) => ({
+          method: 'GET',
+          url: `v1/subscriptions/my-payments?page=${page}&pageSize=${pageSize}`,
+        }),
+      }),
+    }
+  },
+})
+
+export const {
+  useCreateSubscriptionMutation,
+  useGetCurrentSubscriptionQuery,
+  useGetPaymentsQuery,
+} = SubscriptionsApi
